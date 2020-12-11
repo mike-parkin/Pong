3c30ade205286ece16c5bedbbb05838a883b0cd1
Mike Parkin <michael.parkin1988@gmail.com>
2020-12-11 18:24:04 +1300

    Add Timer and Graphic libraries and functions

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -4,14 +4,32 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	
-	private static final Color BACKGROUND_COLOUR = Color.BLACK;
+	private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	private final static int TIMER_DELAY = 5;
 	
 	public PongPanel() {
 		setBackground(BACKGROUND_COLOUR);
+		Timer timer = new Timer(TIMER_DELAY, this);
+			timer.start();
 	}
+	
+	private void update() {
+		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(20, 20, 100, 100);
+	}
+	
 	@Override
 	public void keyTyped(KeyEvent event) {
 		// TODO Auto-generated method stub
@@ -32,8 +50,8 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+		update();
+		repaint();
 	}
 
 }
